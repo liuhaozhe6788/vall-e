ecc6b82b2ccaf392a89f51821a34eed72736aadf
liuhaozhe6788 <2792382045@qq.com>
2023-03-01 16:18:34 +0800

    modify run shell

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-python run_qnt.py data/test 
-python run_g2p.py data/test
-# python run_train.py yaml=config/test/ar.yml
-# python run_train.py yaml=config/test/nar.yml
-# python run_export.py zoo/ar.pt yaml=config/test/ar.yml
-# python run_export.py zoo/nar.pt yaml=config/test/nar.yml
+python run_qnt.py data/LibriTTS
+python run_g2p.py data/LibriTTS
+# python run_train.py yaml=config/LibriTTS/ar.yml
+# python run_train.py yaml=config/LibriTTS/nar.yml
+# python run_export.py zoo/ar.pt yaml=config/LibriTTS/ar.yml
+# python run_export.py zoo/nar.pt yaml=config/LibriTTS/nar.yml
 # python run.py "Well" "/home/liuhaozhe/voice_cloning_project/collected_audios/recorded_audios/liuhaozhe/liuhaozhe_text1.m4a" 
